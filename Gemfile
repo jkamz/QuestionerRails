0e88c1540629b498eae7c0ed1584086e58aafe89
jkamz <jkamz6432@gmail.com>
2019-07-21 23:18:34 +0300

    add jbuilder for json

--- Gemfile
+++ Gemfile
@@ -13,13 +13,14 @@ gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-# gem 'jbuilder', '~> 2.5'
+gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 gem 'simple_token_authentication', '~> 1.0'
+gem 'jwt'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,9 +1,14 @@
 class V1::SessionsController < ApplicationController
   def create
-    user = User.where(email: params[:email]).first
+    @user = User.where(email: params[:email]).first
 
-    if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    if @user&.valid_password?(params[:password])
+      token = JWT.encode(
+        { user_id: @user.id },
+        Rails.application.secrets.secret_key_base,
+        'HS256'
+      )
+      render :create, status: :created
     else
       head(:unauthorized)
     end
