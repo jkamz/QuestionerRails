cc32afe0a6ae1a25c9ced35b133594f4cdaf8cf2
jkamz <jkamz6432@gmail.com>
2019-07-21 19:42:37 +0300

    create db and migrate

--- db/migrate/20190721155027_create_meetups.rb
+++ db/migrate/20190721155027_create_meetups.rb
@@ -1,7 +1,11 @@
 class CreateMeetups < ActiveRecord::Migration[5.1]
   def change
     create_table :meetups do |t|
-
+      t.string :host
+      t.string :topic
+      t.string :summary
+      t.string :location
+      t.datetime :happening_on
       t.timestamps
     end
   end
